64de4e7b1b879b7b564ee634a4dfb79fead99ecd
松栄健太 <matuse.kenta@gmail.com>
2018-12-31 17:59:11 +0900

    ProductコンテナからProductCardを呼べるように設定

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -7,7 +7,18 @@ class Products extends Component {
         return (
             <div>
                 this is the page of products.
-                <ProductCard/>
+                <ProductCard
+                  productName = "Fast Idea"
+                  releaseDate = "2018年8月14日"
+                  imagePath = "/images/fastidea-icon.png"
+                  productSumary = "ここにアプリのようやくを入れる"
+                  />
+                <ProductCard
+                  productName = "Tip Off"
+                  releaseDate = "2018年12月12日"
+                  imagePath = "/images/tipoff-icon.png"
+                  productSumary = "ここにアプリのようやくを入れる"
+                  />
             </div>
         );
     }
